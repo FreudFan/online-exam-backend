526a2e0afdea4679f95e1576bee9af304dad4fde
FreudFan <32455840+freudfan@users.noreply.github.com>
2019-11-26 20:34:08 +0800

    新建项目

---
+++ src/main/java/com/sandau/onlineexam/OnlineExamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sandau.onlineexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineExamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineExamBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sandau/onlineexam/OnlineExamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandau.onlineexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineExamBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
