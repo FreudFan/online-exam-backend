f58d2e45fefd30344bbb94dc65004b466103bd51
FreudFan <moiyangyang@outlook.com>
2019-11-27 00:33:11 +0800

    添加基础配置

---
+++ src/main/java/com/sandau/onlineexam/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.sandau.onlineexam.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+import static org.springframework.web.cors.CorsConfiguration.ALL;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    // 用来允许接受CORS的跨域请求
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedHeaders("*")
+                .allowedOrigins("*")
+                .allowedMethods("GET","POST","PUT","DELETE");
+
+    }
+
+}
