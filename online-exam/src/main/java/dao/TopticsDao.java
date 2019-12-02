46dbeb175d02f6a312df4d4346a4242524eea4f3
wuxiao <825612027@qq.com>
2019-12-02 22:59:48 +0800

    825612027@qq.com

--- online-exam/src/main/java/dao/TopticsDao.java
+++ online-exam/src/main/java/dao/TopticsDao.java
@@ -4,10 +4,7 @@ import datasource.ConnectionManager;
 import datasource.JDBCUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -37,12 +34,21 @@ public class TopticsDao {
         try {
             Connection connection = ConnectionManager.getConnection();
             PreparedStatement stmt = connection.prepareStatement(sbForOptions.toString());
+            ResultSet rs ;
+            PreparedStatement ps = connection.prepareStatement(sb.toString(), Statement.RETURN_GENERATED_KEYS);
             for (int i = 1; i < excelList.size(); i++) {
-                String description = (String) excelList.get(i).get(0);
-                String correctKey = (String) excelList.get(i).get(6);
+                String description = String.valueOf(excelList.get(i).get(0));
+                String correctKey = String.valueOf(excelList.get(i).get(6));
                 Double topicMark = (Double) excelList.get(i).get(7);
-                String analysis = (String) excelList.get(i).get(8);
-                int id = JDBCUtils.getId(sb.toString(), description, correctKey, topicMark, analysis);
+                String analysis = String.valueOf(excelList.get(i).get(8));
+                ps.setString(1, description);
+                ps.setString(2, correctKey);
+                ps.setDouble(3, topicMark);
+                ps.setString(4, analysis);
+                ps.executeUpdate();
+                rs =  ps.getGeneratedKeys();
+                rs.next();
+                int id = rs.getInt(1);
                 for (int j = 1; j < 6; j++) {
                     String option = "";
                     switch (j) {
@@ -65,10 +71,11 @@ public class TopticsDao {
                     if (excelList.get(i).get(j) != "") {
                         stmt.setInt(1, id);
                         stmt.setString(2, option);
-                        stmt.setString(3, (String) excelList.get(i).get(j));
-                        stmt.executeUpdate();
+                        stmt.setString(3, String.valueOf(excelList.get(i).get(j)));
+                        stmt.addBatch();
                     }
                 }
+                stmt.executeBatch();
             }
         }catch (Throwable e){
             flag = false;
