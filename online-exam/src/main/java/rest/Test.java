4446248226ec4ebf2f15aa3accabdcd19db0dcd9
FreudFan <32455840+freudfan@users.noreply.github.com>
2019-12-07 17:16:04 +0800

    接受post请求demo

--- online-exam/src/main/java/rest/Test.java
+++ online-exam/src/main/java/rest/Test.java
@@ -1,19 +1,25 @@
 package rest;
 
 import com.alibaba.fastjson.JSON;
-import datasource.ConnectionManager;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.MapUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Path("test")
 public class Test {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @GET
     @Path("show")
@@ -21,7 +27,7 @@ public class Test {
     @Produces({ MediaType.APPLICATION_JSON })
     public String topicShow() throws Exception {
         String sql = "SELECT * FROM topics";
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(ConnectionManager.getDataSource());
+//        JdbcTemplate jdbcTemplate = new JdbcTemplate(ConnectionManager.getDataSource());
         List list = new ArrayList();
         for ( int i = 0; i < 100000; i++ ) {
             list = jdbcTemplate.queryForList(sql);
@@ -29,6 +35,27 @@ public class Test {
         return JSON.toJSONString(list);
 //        topticsService.read();
 //        return null;
+    }
+
+    @POST
+    @Path("post")
+    @Consumes({ MediaType.APPLICATION_FORM_URLENCODED })    //使用 application/x-www-form-urlencoded
+    @Produces({ MediaType.APPLICATION_JSON })   //使用@FormParam 可接收单个name，如果传的是list可在postman里设多个相同name的参数
+    public String post(@FormParam("key") String value, @FormParam("key1") String value1, @FormParam("key2") List<String> value2 ) throws Exception {
+        log.info("key = " + value + "\tkey1 = " + value1 + "\tkey2 = " + value2);
+        return null;
+    }
+
+    @POST
+    @Path("postlist")
+    @Consumes({ MediaType.APPLICATION_FORM_URLENCODED })
+    @Produces({ MediaType.APPLICATION_JSON })   //使用MultivaluedMap可以接受复杂类型数据，会将所有key的值作为list接收，类似于 Map<List> 类型
+    public String postList(MultivaluedMap map) throws Exception {
+        log.info(map.toString());
+        List l = (List)map.get("key");
+        String s = (String) l.get(0);
+        log.info(s);
+        return null;
     }
 
 }
