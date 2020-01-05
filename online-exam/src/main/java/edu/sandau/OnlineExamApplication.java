096307605f9adb69175d721ed4cc6c573bed7d83
FreudFan <32455840+freudfan@users.noreply.github.com>
2020-01-05 16:32:44 +0800

    SLF4J接管JUL

--- online-exam/src/main/java/edu/sandau/OnlineExamApplication.java
+++ online-exam/src/main/java/edu/sandau/OnlineExamApplication.java
@@ -1,27 +1,16 @@
 package edu.sandau;
 
-import edu.sandau.datasource.DruidManager;
-import lombok.extern.slf4j.Slf4j;
 import edu.sandau.tomcat.Tomcat8;
+import org.slf4j.bridge.SLF4JBridgeHandler;
 
-@Slf4j
 public class OnlineExamApplication {
     public static void main(String[] args) throws Exception {
+        SLF4JBridgeHandler.install();
         Thread tomcat8 = new Thread(new Tomcat8());
         tomcat8.start();
         init();
     }
 
-    private static void init() throws Exception {
-//        initDatabaseConnectionPool();
-    }
-
-    /***
-     * 初始化连接池
-     */
-    private static void initDatabaseConnectionPool() {
-        java.sql.Connection connection = DruidManager.getConnection();
-        DruidManager.closeAll(connection,null,null);
-    }
+    private static void init() throws Exception {}
 
 }
