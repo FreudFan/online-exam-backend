aa3d0c99892da5a99498afa36815fce197b92c51
FreudFan <moiyangyang@outlook.com>
2020-02-10 21:43:22 +0800

    通过线程池启动tomcat

--- online-exam/src/main/java/edu/sandau/OnlineExamApplication.java
+++ online-exam/src/main/java/edu/sandau/OnlineExamApplication.java
@@ -3,14 +3,14 @@ package edu.sandau;
 import edu.sandau.tomcat.Tomcat8;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class OnlineExamApplication {
     public static void main(String[] args) throws Exception {
-//        SLF4JBridgeHandler.install();
-        Thread tomcat8 = new Thread(new Tomcat8());
-        tomcat8.start();
-        init();
+        SLF4JBridgeHandler.install();
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(new Tomcat8());
     }
-
-    private static void init() throws Exception {}
 
 }
